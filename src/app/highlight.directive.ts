7aa12515ead2872794651a2cc7ac41b125938dad
marion vutagwa <marionvutagwa@yahoo.com>
2020-06-18 16:51:16 +0300

    add highlight directive to the application

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef,HostListener} from '@angular/core';
 
 @Directive({
   selector: '[apphighlight]'
 })
 export class highlightDirective {
 
-  constructor() { }
+    
+  constructor(private elem:ElementRef){}
 
+  @HostListener("click") onClicks(){
+    this.textDeco("highlight")
+  }
+
+  @HostListener("dblclick") onDoubleClicks(){
+    this.textDeco("None")
+  }
+
+  private textDeco(action:string){
+    this.elem.nativeElement.style.textDecoration=action;
+
+  }
 }
