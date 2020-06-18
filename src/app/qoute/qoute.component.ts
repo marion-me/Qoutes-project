9cb4209896880439e113170021e7fcc3c6c057fd
marion vutagwa <marionvutagwa@yahoo.com>
2020-06-18 03:09:13 +0300

    add the qoute component to make qoute notation easy for the application

--- src/app/qoute/qoute.component.ts
+++ src/app/qoute/qoute.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./qoute.component.css']
 })
 export class QouteComponent implements OnInit {
+  author: Text
+  Qoute: Text
   upvotes: number
   numberOfUpvotes: any;
   numberOfdownvotes: any;
