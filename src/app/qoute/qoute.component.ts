e171952b7d9440ffeb690d3d93a1188a8747507e
marion vutagwa <marionvutagwa@yahoo.com>
2020-06-18 17:18:07 +0300

    add all new changes made

--- src/app/qoute/qoute.component.ts
+++ src/app/qoute/qoute.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./qoute.component.css']
 })
 export class QouteComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+  }
+  [x: string]: any;
   author: Text
   Qoute: Text
   upvotes: number
@@ -20,9 +24,14 @@ export class QouteComponent implements OnInit {
     this.numberOfdownvotes--;
   }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+        deleteGoal(isComplete: any, index: string | number){
+          if (isComplete) {
+            let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+      
+            if (toDelete){
+              this.qoute.splice(index,1)
+            }
+          }
+        }
+      
+      }
